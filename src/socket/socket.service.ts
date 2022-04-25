d1c29a46afa86f549ccca60c0076338e489981e4
j.zaripov <zaripovjasurbek2001@gmail.com>
2022-04-25 10:24:12 +0500

    added chat

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,14 +1,24 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-
 @WebSocketGateway()
 export class SocketService {
     @WebSocketServer()
     server: Server;
+    messages = [];
 
+    @SubscribeMessage('name:get')
+    getName(@ConnectedSocket() client: any) {
+        this.server.emit('name:send', client.id);
+    }
     @SubscribeMessage('moved')
     handleDate(@MessageBody() body: SocketBody) {
         this.server.emit('update', body);
+    }
+
+    @SubscribeMessage('message')
+    handleMessage(@MessageBody() body: chatBody) {
+        if (body && body.name) this.messages.push({ name: body.name, message: body.message, time: body.time });
+        this.server.emit('messages', this.messages);
     }
 }
 export interface SocketBody {
@@ -17,4 +27,9 @@ export interface SocketBody {
     figure: string;
     player: string;
 
+}
+export interface chatBody {
+    name: string,
+    message: string;
+    time: string;
 }
