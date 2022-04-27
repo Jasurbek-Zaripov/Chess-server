78cd6c8ee9297938ef221c7853b670e19d0dec25
j.zaripov <zaripovjasurbek2001@gmail.com>
2022-04-27 09:39:54 +0500

    mybe end

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,24 +1,57 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 @WebSocketGateway()
-export class SocketService {
+export class SocketService implements OnGatewayDisconnect, OnGatewayConnection {
     @WebSocketServer()
     server: Server;
     messages = [];
+    users = [];
+    id: any;
+
+    constructor() {
+    }
+
+    handleConnection(client: Socket) {
+        this.users.push(client.id);
+    }
 
     @SubscribeMessage('name:get')
-    getName(@ConnectedSocket() client: any) {
+    getName(@ConnectedSocket() client: Socket) {
         this.server.emit('name:send', client.id);
     }
+    @SubscribeMessage('write')
+    onWrite(@ConnectedSocket() client: Socket) {
+        this.users.forEach(id => {
+            if (id != client.id) {
+                this.server.to(id).emit('writed');
+            }
+        });
+    }
+
     @SubscribeMessage('moved')
     handleDate(@MessageBody() body: SocketBody) {
         this.server.emit('update', body);
     }
-
     @SubscribeMessage('message')
     handleMessage(@MessageBody() body: chatBody) {
-        if (body && body.name) this.messages.push({ name: body.name, message: body.message, time: body.time });
+        if (body && body.name) {
+            this.messages.push({ name: body.name, message: body.message, time: body.time });
+            if (!this.id) {
+                this.id = setInterval(() => {
+                    if (this.messages.shift()) this.server.emit('messages', this.messages);
+                    else {
+                        clearInterval(this.id);
+                        this.id = null;
+                    }
+                }, 10000);
+            }
+        }
         this.server.emit('messages', this.messages);
+    }
+
+    handleDisconnect(client: Socket) {
+        this.users = this.users.filter(id => id != client.id);
+        this.server.emit('exit');
     }
 }
 export interface SocketBody {
